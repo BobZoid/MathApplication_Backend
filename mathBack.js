56f2d5186e5a3d31630e36a30e00a5bd147817e9
Causality <m.uddner@gmail.com>
2021-10-21 12:53:33 +0200

    add db to main folder, connect backend with frontend

--- mathBack.js
+++ mathBack.js
@@ -1,29 +1,35 @@
-var express = require("express")
-var app = express()
-var cors = require('cors')
+let express = require("express")
+let app = express()
+let cors = require('cors')
 
 app.use(cors())
 app.use(express.static('public'))
 
-var bodyParser = require("body-parser")
+let bodyParser = require("body-parser")
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-var HTTP_PORT = 3030
+let db = require("./database.js")
+
+let HTTP_PORT = 3030
 
 app.listen(HTTP_PORT, () => {
     console.log("MathKnight Backend kör på port %PORT%".replace("%PORT%",HTTP_PORT))
 });
 
-app.get("/users", (req, res) => {
+app.get("/students", (req, res) => {
 
-    let sql = "select * from Users"
+    let sql = "select * from students"
     let params = []
 
     db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({"error": err.message});
+            return;
+        }
         res.json({
-            "message": "Successfully imported users.",
-            "user": rows
+            "message": "Successfully imported database.",
+            "students": rows
         })
     });
 });
